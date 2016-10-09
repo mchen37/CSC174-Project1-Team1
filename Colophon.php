1535abe2491d16989cf519d193c82b999673031f
novaskyland <nick.heitsch@gmail.com>
2016-10-09 17:35:32 -0400

    Fixed html and applied CSS to Colophon

--- Colophon.php
+++ Colophon.php
@@ -5,20 +5,29 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+	<link rel="stylesheet" href="css/side-menu.css">
 	<title> Project 1- Colophon </title>
 </head>
 
-<body>
+<body id="layout">
 
 <?php
 include "includes/nav.inc";
 ?>
+<div class="main">
+	
+		
+		<header class="header">
+			<h1> Team Roles </h1>
+		</header>
+	<div class="content">
 
 	<article>
 	</article>
 	
 	<aside>
-		<h3> Team Roles </h3>
 		<ul>
 			<li> Steven Lebowski = The Architect </li>
 			<li> Jorge Sanchez De Llano = The Framework Wrangler </li>
@@ -28,6 +37,6 @@ include "includes/nav.inc";
 		</ul>
 	</aside>	
 
-	
+<script src="scripts/ui.js"></script>	
 </body>
 </html>
