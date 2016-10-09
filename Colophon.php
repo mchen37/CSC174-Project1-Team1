448039cbd8f3240b5c88ce8174fbb87a9aa6b391
dbrown52 <dbrown52@u.rochester.edu>
2016-10-09 11:37:05 -0400

    Update Colophon.php

--- Colophon.php
+++ Colophon.php
@@ -14,9 +14,20 @@
 include "includes/nav.inc";
 ?>
 
-<!-- Insert text here to fill out-->
+	<article>
+	</article>
+	
+	<aside>
+		<h3> Team Roles </h3>
+		<ul>
+			<li> Steven Lebowski = The Architect </li>
+			<li> Jorge Sanchez De Llano = The Framework Wrangler </li>
+			<li> Davon Brown = The Content Master and Designer </li>
+			<li> Nicholas Heitsch = The CSS Artist </li>
+			<li> Connor Dent = The Javacsript Coder </li>
+		</ul>
+	</aside>	
 
-
-
+	
 </body>
 </html>
