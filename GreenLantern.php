265bce39a156308a6e67393647b61836e5bd7766
novaskyland <nick.heitsch@gmail.com>
2016-10-11 15:55:12 -0400

    Css edits

--- GreenLantern.php
+++ GreenLantern.php
@@ -26,7 +26,7 @@ include "includes/nav.inc";
 	<div class="content">
 	<article>
 		<h2 class="content-subhead"> History </h2>
-		<img id = "gl" src = "img/gl.png">
+		<img id = "gl" class="image" src = "img/gl.png">
 		<p> Green Lantern is the name of a number of superheroes appearing in American comic books published by DC Comics. They fight evil with the aid of rings that grant them a variety of extraordinary powers.
 		The first Green Lantern character, Alan Scott, was created in 1940 during the initial popularity of superheroes. Alan Scott usually fought common criminals in New York City with the aid of his magic ring. The publication of this character ceased in 1949 during a general decline in the popularity of superhero comics, but the character saw a limited revival in later decades.
 		In 1959, to capitalize on the booming popularity of science fiction, the Green Lantern character was reinvented as Hal Jordan, an officer for an interstellar law enforcement agency known as the Green Lantern Corps. Additional members of this agency, all of whom call themselves Green Lanterns, were introduced over time. Prominent Green Lanterns who also have had starring roles in the books include Guy Gardner, John Stewart, Kyle Rayner, Simon Baz and Jessica Cruz.
@@ -41,7 +41,7 @@ include "includes/nav.inc";
 
 	<article>
 		<h2 class="content-subhead"> Story </h2>
-		<img id = "hal" src = "img/hal.png">
+		<img id = "hal" class="image" src = "img/hal.png">
 		<p> Hal Jordan is the greatest Green Lantern, an inter-galactic police officer and member of the Green Lantern Corps, chosen by the Guardians of the Universe for his ability to overcome great fear. The strength of his willpower allows him to wield the universe's mightiest weapon, a power ring controlled by his thoughts. In his secret identity he is a test pilot working for Ferris Aircraft where his boss Carol Ferris is also his romantic interest. Having turned into the evil Parallax during Emerald Twilight and died during Final Night, his soul became a host of the Spectre during Day of Judgment until his resurrection during Green Lantern: Rebirth. Although he is the most well-known Lantern, Alan Scott of the Golden Age is the original version of the character. Other Lanterns that have acted as his partners, successors or replacements at times have included Guy Gardner, John Stewart and Kyle Rayner. He is a founding member of the Justice League of America.
 		</p>
 	</article>
--- Thor.php
+++ Thor.php
@@ -23,7 +23,7 @@ include "includes/nav.inc";
 
 	<article>
 		<h2 class="content-subhead"> History </h2>
-		<img id = "thor" src = "img/thor_history.png">
+		<img id = "thor" class="image" src = "img/thor_history.png">
 		<p> Thor is a fictional superhero appearing in American comic books published by Marvel Comics. The character, based on the Norse mythological deity of the same name, is the Asgardian god of thunder and possesses the enchanted hammer Mjolnir, which grants him the ability of flight and weather manipulation amongst his other superhuman attributes. Debuting in the Silver Age of Comic Books, the character first appeared in Journey into Mystery #83 (Aug. 1962) and was created by editor-plotter Stan Lee, scripter Larry Lieber, and penciller-plotter Jack Kirby. He has starred in several ongoing series and limited series, and is a founding member of the superhero team the Avengers, appearing in each volume of that series.
 		</p>
 	</article>
@@ -47,7 +47,7 @@ When Mjolnir was damaged, Doctor Strange bound Thor's soul into Mjolnir, meaning
 
 	<article>
 		<h2 class="content-subhead"> Story </h2>
-		<img id = "god" src = "img/thor2.png">
+		<img id = "god" class="image" src = "img/thor2.png">
 		<p> Thor's father Odin decides his son needed to be taught humility and consequently places Thor (without memories of godhood) into the body and memories of an existing, partially disabled human medical student, Donald Blake. After becoming a doctor and on vacation in Norway, Blake witnesses the arrival of an alien scouting party. Blake flees from the aliens into a cave. After discovering Thor's hammer Mjolnir (disguised as a walking stick) and striking it against a rock, he transforms into the thunder god. Later, in Thor #159, Blake is revealed to have always been Thor, Odin's enchantment having caused him to forget his history as The Thunder God and believe himself mortal. Defeating the aliens, Thor shares a double life with his alter ego: treating the ill in a private practice with nurse - and eventual love - Jane Foster, and defending humanity from evil.
 		</p>
 	</article>
