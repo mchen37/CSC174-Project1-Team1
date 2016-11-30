90c9557438bdcfcce03322fe5bc8998d70e7ece6
Moses Chen <mchen37@u.rochester.edu>
2016-11-30 15:32:28 -0500

    Fix file case

---
+++ colophon.php
@@ -0,0 +1,56 @@
+<?php $parentTitle = "SuperHero Central"; $currentTitle = "Colophon"; $separator = " | "; $currentBody = "body_blue"; include "inc/top.inc";?>
+
+		<?php include "inc/nav.inc";?>
+
+		<article>
+
+			<h2><div class="retro">Colophon</div></h2>
+
+			<h3 class="inset-text-effect">What we do</h3>
+
+			<p>We are CSC 174 students who would like to create a reference website for all Super Hero lovers.
+
+			<h3 class="inset-text-effect">Who we are</h3>
+
+			<ul>
+				<li>Steven Lebowski - Architect</li>
+				<li>Jorge Sanchez De Llano - Framework wrangler</li>
+				<li>Davon Brown - Content master and interaction designer</li>
+				<li>Nicholas Heitsch - CSS artist</li>
+				<li>Connor Dent - JavaScript engineer</li>
+			</ul>
+
+			<h3 class="inset-text-effect">Technologies used</h3>
+				
+			<ul>
+				<li>Built on Bootstrap framework</li>
+				<li>Based on SMACSS (Scalable and Modular Architecture for CSS), with <a href="https://necolas.github.io/normalize.css/">"normalize.css"</a></li>
+				<li>Overridden Bootstrap CSS with custom CSS in "styles.css"</li>
+				<li>Embedded a JavaScript-powered slideshow, using Bootstrap Carousel plugin</li>
+				<li>Employed the use of PHP includes</li>
+				<li>Worked with <a href="https://github.com/theSKML01/CSC174-Project1-Team1">a shared repository on Github</a></li>
+				<li>Employed a solution for the current menu highlight problem</li>
+				<li>Supported mobile website view using CSS media queries</li>
+				<li>Embedded Google font "Cuprum"</li>
+			</ul>
+
+			<h3 class="inset-text-effect">Source cited</h3>
+
+			<p>We took everything from Wikipedia, and like Prof Kostin, will return it to where it belongs. To be honest, we also used DC Wiki to provide Green Lantern's Story section.</p>
+
+			<ul>
+				<li>Wikipedia</li>
+				<li>dc.wikia.com</li>
+			</ul>
+
+		</article>
+			
+		<?php include "inc/footer.inc";?>
+
+	</div><!--.container-->
+	
+	<?php include "inc/scripts.inc";?>
+	
+	</body>
+
+</html>
---
+++ greenlantern.php
@@ -0,0 +1,49 @@
+<?php $parentTitle = "SuperHero Central"; $currentTitle = "Green Lantern"; $separator = " | "; $currentBody = "body_green"; include "inc/top.inc";?>
+
+		<?php include "inc/nav.inc";?>
+
+		<article>
+
+			<h2><div class="retro">Green Lantern</div></h2>
+
+			<div class="content">
+
+			<h3 class="inset-text-effect">History</h3>
+
+			<img src="images/gl.png" alt="gl">
+
+			<p><strong>Green Lantern</strong> is the name of <strong>a number of superheroes</strong> appearing in American comic books published by DC Comics. They fight evil with the aid of rings that grant them a variety of extraordinary powers.</p>
+
+			<p>The first Green Lantern character, Alan Scott, was created in 1940 during the initial popularity of superheroes. Alan Scott usually fought common criminals in New York City with the aid of his magic ring. The publication of this character ceased in 1949 during a general decline in the popularity of superhero comics, but the character saw a limited revival in later decades.</p>
+
+			<p>In 1959, to capitalize on the booming popularity of science fiction, the Green Lantern character was reinvented as Hal Jordan, an officer for an interstellar law enforcement agency known as the Green Lantern Corps. Additional members of this agency, all of whom call themselves Green Lanterns, were introduced over time. Prominent Green Lanterns who also have had starring roles in the books include Guy Gardner, John Stewart, Kyle Rayner, Simon Baz and Jessica Cruz.</p>
+
+			<p>The Green Lanterns are among DC Comics' more successful properties. They have been adapted to television, video games, and motion pictures.</p>
+
+			<h3 class="inset-text-effect">Story</h3>
+
+			<img src="images/hal.png" alt="hal">
+
+			<p><strong>Hal Jordan</strong> is the greatest Green Lantern, an inter-galactic police officer and member of the Green Lantern Corps, chosen by the Guardians of the Universe for his ability to overcome great fear. The strength of his willpower allows him to wield the universe's mightiest weapon, a power ring controlled by his thoughts. In his secret identity he is a test pilot working for Ferris Aircraft where his boss Carol Ferris is also his romantic interest. Having turned into the evil Parallax during Emerald Twilight and died during Final Night, his soul became a host of the Spectre during Day of Judgment until his resurrection during Green Lantern: Rebirth. Although he is the most well-known Lantern, Alan Scott of the Golden Age is the original version of the character. Other Lanterns that have acted as his partners, successors or replacements at times have included Guy Gardner, John Stewart and Kyle Rayner. He is a founding member of the Justice League of America.</p>
+				
+			</div>
+
+		</article>
+
+		<aside>
+			
+			<h3 class="inset-text-effect">Powers</h3>
+
+			<p>Use of power ring grants numerous powers, including flight, invulnerability, omnilingualism, and the generation of hard-light constructs</p>
+
+		</aside>
+
+		<?php include "inc/footer.inc";?>
+
+	</div><!--.container-->
+	
+	<?php include "inc/scripts.inc";?>
+	
+	</body>
+
+</html>
