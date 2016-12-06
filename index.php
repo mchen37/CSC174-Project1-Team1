0c68cf91b82ab4af578399e612faeac9ea8e5148
mtam02 <mtam2@u.rochester.edu>
2016-12-06 16:57:17 -0500

    container fix

--- index.php
+++ index.php
@@ -2,47 +2,42 @@
 
 		<?php include "inc/nav.inc";?>
 
-		<!-- Slideshow begin -->
-		<div class="container">
-
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Slideshow -->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
 				
-				<!-- indicators -->
-				<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-				</ol>
+			<!-- indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
 
-				<!-- Wrapper for slides -->
-				<div class="carousel-inner" role="listbox">
-					<div class="item active">
-						<img src="images/spidey.png" alt="spider" width="600" height="480">
-					</div>
-					<div class="item">
-						<img src="images/greenlantern.png" alt="spider" width="600" height="480">
-					</div>
-					<div class="item">
-						<img src="images/thor.png" alt="spider" width="600" height="480">
-					</div>
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<img src="images/spidey.png" alt="spider" width="600" height="480">
 				</div>
-
-				<!-- Left and right controls -->
-
-				<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-
-				<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-
+				<div class="item">
+					<img src="images/greenlantern.png" alt="spider" width="600" height="480">
+				</div>
+				<div class="item">
+					<img src="images/thor.png" alt="spider" width="600" height="480">
+				</div>
 			</div>
 
-		</div>
-		<!-- End slideshow -->
+			<!-- Left and right controls -->
+
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+
+		</div><!-- Slideshow -->
 
 		<h2><div class="deepshadow">Superheroes</div></h2>
 
